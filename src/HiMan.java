e96ed2019494f3ca78c7c633b87b01bc687b4927
ChainSawUser <gladky.sergey@gmail.com>
2022-08-15 12:57:47 +0300

    Init

---
+++ src/HiMan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HiMan {
+
+	public static void main(String[] args) {
+		Scanner userVal = new Scanner (System.in);
+		int i;
+		
+		i = userVal.nextInt();
+		System.out.print("Your variable is: " + i);
+	}
+
+}
